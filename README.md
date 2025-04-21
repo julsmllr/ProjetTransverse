2b99a6e19685c4f4e3cc85fbb40c7feac33bb87d
Jules MULLER <julesmuller06@gmail.com>
2025-04-21 19:47:00 +0200

    Finalisation Design Entrées

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # ProjetTransverse
 
+
+# Pour mieux comprendre le code : 
+- le pack_propagate(False) permet d'avoir toujours 640px pour chaque section sinon le container prend la taille de son contenu
 # Instruction pour Jeu Pygame : 
 - Dimension : 640x1080
--- interface/interface.py
+++ interface/interface.py
@@ -1,109 +1,222 @@
-
-# ------------ IMPORTATIONS LIBRAIRIES ------------ #
+# =============================================================================
+# IMPORTATIONS
+# =============================================================================
 import tkinter as t
 from tkinter import font
+import os
 
-from numpy import pad
+# =============================================================================
+# CONSTANTES ET VARIABLES GLOBALES
+# =============================================================================
 
-# ------------ VARIABLES GLOBALES ------------ #
-
-
-# Couleurs de l'interface
+# Couleurs
 bg_color = "#F8F8F8"
 primary_color = "#FCA311"
 text_color = "#14213D"
-
 dark_bg_color = "#213241"
 dark_primary_color = "#FC8E11"
 dark_text_color = "#F8F8F8"
 
+# Polices
+primaryFont = ("Alata-Regular", 20)
 
-# Initialisation de la police par défaut
+# =============================================================================
+# GESTIONNAIRES D'ÉVÉNEMENTS
+# =============================================================================
 
+def RightButtonClicked(event):
+    gestionSettingsEntriesDirectionButtonPlus.config(image=GameSettingDirectionRightClicked)
 
+def RightButtonNotClicked(event):
+    gestionSettingsEntriesDirectionButtonPlus.config(image=GameSettingDirectionRight)
 
-# ------------ INITIALISATION FENETRE ------------ #
+def LeftButtonClicked(event):
+    gestionSettingsEntriesDirectionButtonMinus.config(image=GameSettingDirectionLeftClicked)
 
+def LeftButtonNotClicked(event):
+    gestionSettingsEntriesDirectionButtonMinus.config(image=GameSettingDirectionLeft)
+
+def LaunchButtonNotClicked(event):
+    gestionSettingsConfirmationButton.config(image=BoutonLancer)
+
+def LaunchButtonClicked(event):
+    gestionSettingsConfirmationButton.config(image=BoutonLancerClicked)
+# =============================================================================
+# CONFIGURATION INITIALE
+# =============================================================================
+
+# Création de la fenêtre principale
 jeu = t.Tk()
-
-
 jeu.title("Simulation de projectile")
 jeu.geometry("1920x1080")
 jeu.attributes('-fullscreen', True)
-jeu.bind('<Escape>',lambda e: jeu.destroy()) # Pour quitter l'application avec la touche "Echap"
+jeu.bind('<Escape>', lambda e: jeu.destroy())
 
+# Configuration de la police
+font_path = "../assets/fonts/Alata-Regular.ttf"
+tk_font = font.Font(family="Alata-Regular", size=12)
+jeu.tk.call("font", "create", "Alata-Regular", "-family", "Alata-Regular", "-size", "12")
 
+# Chargement des images
+GameSettingDirectionLeft = t.PhotoImage(file="../assets/buttonImg/GameSettingDirectionLeft.png")
+GameSettingDirectionRight = t.PhotoImage(file="../assets/buttonImg/GameSettingDirectionRight.png")
+GameSettingDirectionLeftClicked = t.PhotoImage(file="../assets/buttonImg/GameSettingsDirectionLeftClicked.png")
+GameSettingDirectionRightClicked = t.PhotoImage(file="../assets/buttonImg/GameSettingsDirectionRightClicked.png")
+BoutonLancer = t.PhotoImage(file="../assets/buttonImg/LancerBouton.png")
+BoutonLancerClicked = t.PhotoImage(file="../assets/buttonImg/LancerBoutonClicked.png")  
+
+# =============================================================================
+# CRÉATION DE L'INTERFACE
+# =============================================================================
+
+# Frame principale
 gameInterface = t.Frame(jeu, bg=dark_bg_color)
 gameInterface.pack(expand=True, fill="both")
 
-# Organisation de l'interface
-gameGestionSettings = t.Frame(gameInterface, bg=bg_color)
-gameInterfaceMiddle = t.Frame(gameInterface, bg='blue')
-gameInformations = t.Frame(gameInterface, bg=bg_color)
-gameGestionSettings.pack(side="left", fill="both", expand=True)   
+# Sections principales
+gameGestionSettings = t.Frame(gameInterface, bg=bg_color, width=640)
+gameInterfaceMiddle = t.Frame(gameInterface, bg='red', width=640)
+gameInformations = t.Frame(gameInterface, bg='purple', width=640)
+
+gameGestionSettings.pack_propagate(False)
+gameInterfaceMiddle.pack_propagate(False)
+gameInformations.pack_propagate(False)
+
+gameGestionSettings.pack(side="left", fill="both", expand=True)
 gameInterfaceMiddle.pack(side="left", fill="both", expand=True)
 gameInformations.pack(side="left", fill="both", expand=True)
 
+# =============================== SECTION GESTION DES PARAMÈTRES =============================== #
 
+# Frames principaux des paramètres
+gestionSettingsFrame = t.Frame(gameGestionSettings, bg=bg_color)
+gestionSettingsFrame.pack(expand=True, fill="both", padx=20, pady=20)
 
-# ------------ Gestion Settings ------------ #
-
-# Frames
-gestionSettingsFrame = t.Frame(gameGestionSettings, bg='purple')
-gestionSettingsFrame.pack(expand=True, fill="both", padx=20, pady=20)   
-
-gestionSettingsEntriesFrame = t.Frame(gestionSettingsFrame, bg=bg_color)
-gestionSettingsConfirmationFrame = t.Frame(gestionSettingsFrame, bg='yellow')
+gestionSettingsEntriesFrame = t.Frame(gestionSettingsFrame, bg=bg_color, borderwidth=2, relief="groove")
+gestionSettingsConfirmationFrame = t.Frame(gestionSettingsFrame, bg=bg_color)
 gestionSettingsEntriesFrame.pack(expand=True, fill="both")
 gestionSettingsConfirmationFrame.pack(fill="both", ipady=100, pady=10, padx=10)
 
-# Frame Game Settings
-gestionSettingsEntriesTitle = t.Frame(gestionSettingsEntriesFrame, bg='red')
-gestionSettingsEntriesDirection = t.Frame(gestionSettingsEntriesFrame, bg='green')
-gestionSettingsEntriesPower = t.Frame(gestionSettingsEntriesFrame, bg='blue')
+# Entrées des settings du jeu
+gestionSettingsEntriesTitle = t.Frame(gestionSettingsEntriesFrame, bg=bg_color)
+gestionSettingsEntriesDirection = t.Frame(gestionSettingsEntriesFrame, bg=bg_color)
+gestionSettingsEntriesPower = t.Frame(gestionSettingsEntriesFrame, bg=bg_color)
 
+gestionSettingsEntriesTitle.pack(fill="both", padx=20, pady=20)
+gestionSettingsEntriesDirection.pack(expand=True, fill="both", padx=20, pady=20)
+gestionSettingsEntriesPower.pack(expand=True, fill="both", padx=20, pady=20)
 
-gestionSettingsEntriesTitle.pack(fill="both")
-gestionSettingsEntriesDirection.pack(expand=True, fill="both")
-gestionSettingsEntriesPower.pack(expand=True, fill="both")
-
-# Frame Game Settings Entries
-gestionSettingsEntriesTitleLabel = t.Label(gestionSettingsEntriesTitle, text="Ajustez votre tir :", fg=text_color, font=("Arial", 20))
+# Titre
+gestionSettingsEntriesTitleLabel = t.Label(
+    gestionSettingsEntriesTitle,
+    text="Ajustez votre tir :",
+    fg=text_color,
+    font=primaryFont
+)
 gestionSettingsEntriesTitleLabel.pack(expand=True)
 
-gestionSettingsEntriesDirectionLabel = t.Label(gestionSettingsEntriesDirection, text="Angle de tir :", fg=text_color, font=("Arial", 20))
+# Section Direction
+gestionSettingsEntriesDirectionLabel = t.Label(
+    gestionSettingsEntriesDirection,
+    text="Angle de tir :",
+    fg=text_color,
+    font=primaryFont
+)
 gestionSettingsEntriesDirectionLabel.pack(side="top", pady=50)
 
-# Frame Game Settings Entries Direction
-gestionSettingsEntriesDirectionButtonFrame = t.Frame(gestionSettingsEntriesDirection, bg='orange')
+gestionSettingsEntriesDirectionButtonFrame = t.Frame(gestionSettingsEntriesDirection, bg=bg_color)
 gestionSettingsEntriesDirectionButtonFrame.pack(expand=True, fill="both", side="bottom")
 
-gestionSettingsEntriesDirectionButtonPlus = t.Button(gestionSettingsEntriesDirectionButtonFrame, text=">", bg=primary_color, fg=text_color, font=("Arial", 20))
-gestionSettingsEntriesDirectionButtonMinus = t.Button(gestionSettingsEntriesDirectionButtonFrame, text="<", bg=primary_color, fg=text_color, font=("Arial", 20))
+gestionSettingsEntriesDirectionButtonPlus = t.Button(
+    gestionSettingsEntriesDirectionButtonFrame,
+    image=GameSettingDirectionRight,
+    bd=0,
+    highlightthickness=0,
+    relief="flat",
+    bg=bg_color,
+    activebackground=bg_color,
+    cursor="hand2"
+)
+
+gestionSettingsEntriesDirectionButtonMinus = t.Button(
+    gestionSettingsEntriesDirectionButtonFrame,
+    image=GameSettingDirectionLeft,
+    bd=0,
+    highlightthickness=0,
+    relief="flat",
+    bg=bg_color,
+    activebackground=bg_color,
+    cursor="hand2"
+)
+
+# Configuration des événements des boutons
+gestionSettingsEntriesDirectionButtonPlus.bind("<Button-1>", RightButtonClicked)
+gestionSettingsEntriesDirectionButtonPlus.bind("<ButtonRelease-1>", RightButtonNotClicked)
+gestionSettingsEntriesDirectionButtonMinus.bind("<Button-1>", LeftButtonClicked)
+gestionSettingsEntriesDirectionButtonMinus.bind("<ButtonRelease-1>", LeftButtonNotClicked)
+
+# Positionnement des boutons
 gestionSettingsEntriesDirectionButtonPlus.pack(side="right", expand=True)
 gestionSettingsEntriesDirectionButtonMinus.pack(side="left", expand=True)
 
-# Frame Game Settings Entries Power
-gestionSettingsEntriesPowerLabel = t.Label(gestionSettingsEntriesPower, text="Puissance de tir :", fg=text_color, font=("Arial", 20))
-gestionSettingsEntriesPowerLabel.pack(side="top", pady=50)
+# =============================== SECTION PUISSANCE =============================== #
 
+gestionSettingsEntriesPowerLabel = t.Label(
+    gestionSettingsEntriesPower,
+    text="Puissance de tir :",
+    fg=text_color,
+    font=primaryFont
+)
+gestionSettingsEntriesPowerLabel.pack(side="top", pady=50)
 
 mon_slider = t.Scale(gestionSettingsEntriesPower, from_=0, to=100, orient='horizontal')
 mon_slider.pack(expand=True, side="bottom")
 
-# Frame Game Settings  Confirmation
-gestionSettingsConfirmationButton = t.Button(gestionSettingsConfirmationFrame, text="Lancer", bg=primary_color, fg=text_color, font=("Arial", 20), width=20)
+# =============================== CONFIRMATION BUTTON =============================== #
+
+gestionSettingsConfirmationButton = t.Button(
+    gestionSettingsConfirmationFrame,
+    image=BoutonLancer,
+    bd=0,
+    highlightthickness=0,
+    relief="flat",
+    bg=bg_color,
+    activebackground=bg_color
+)
 gestionSettingsConfirmationButton.pack(expand=True, ipady=20)
 
 
-# Frame Game
+gestionSettingsConfirmationButton.bind("<Button-1>", LaunchButtonClicked)
+gestionSettingsConfirmationButton.bind("<ButtonRelease-1>", LaunchButtonNotClicked)
 
-GameLabel = t.Label(gameInterfaceMiddle, text="Espace pour le jeu pygame", bg='red', fg=text_color, font=("Arial", 20))
+
+
+# =============================================================================
+# SECTION JEU
+# =============================================================================
+
+GameLabel = t.Label(
+    gameInterfaceMiddle,
+    text="Espace pour le jeu pygame",
+    bg=bg_color,
+    fg=text_color,
+    font=primaryFont
+)
 GameLabel.pack(expand=True)
 
-# Frame Game Informations
+# =============================================================================
+# SECTION INFORMATIONS
+# =============================================================================
+
+score = 0
+scoreLabel = t.Label(
+    gameInformations,
+    text="Score : " + str(score),
+    bg=bg_color,
+    fg=text_color,
+    font=primaryFont
+)
+scoreLabel.pack(expand=True)
 
 if __name__ == "__main__":
     jeu.mainloop()
-
-
